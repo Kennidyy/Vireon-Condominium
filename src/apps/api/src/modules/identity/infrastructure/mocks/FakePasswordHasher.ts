4e82d91230417469eb32f555763efa75915183c3
Nikollas Kennidy <kennidynikollas.dev@gmail.com>
2026-07-21 20:50:30 -0300

    test(api/identity): add CreateUserUseCase unit test

---
+++ src/apps/api/src/modules/identity/infrastructure/mocks/FakePasswordHasher.ts
@@ -0,0 +1,9 @@
+import { PasswordHasher } from '../../application/ports/PasswordHasher';
+
+export class FakePasswordHasher implements PasswordHasher {
+  hash(value: string): Promise<string> {
+    const pswd = `$hashed_${value}`;
+
+    return Promise.resolve(pswd);
+  }
+}
