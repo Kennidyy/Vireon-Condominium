ca714ddd8a2285ec44c42e79d8340d0e49bd2135
Nikollas Kennidy <kennidynikollas.dev@gmail.com>
2026-07-21 19:45:26 -0300

    test(api): add User entity unit test

---
+++ src/apps/api/src/modules/identity/domain/entities/User.spec.ts
@@ -0,0 +1,43 @@
+import { User } from './User';
+import { Email } from '../value-objects/Email';
+import { Password } from '../value-objects/Password';
+
+describe('User Entity', () => {
+  it('should create a user', () => {
+    const email = Email.create('niko@gmail.com');
+    const password = Password.create('Senha123@!');
+
+    const user = User.create('user-id-123', email, password);
+
+    expect(user).toBeInstanceOf(User);
+  });
+
+  it('should assign the user id', () => {
+    const email = Email.create('baa@gmail.com');
+    const password = Password.create('StrongPass123!');
+
+    const user = User.create('user-id-321', email, password);
+
+    expect(user.id).toBe('user-id-321');
+  });
+
+  it('should assign the email value object', () => {
+    const email = Email.create('bubu@hotmail.com');
+    const password = Password.create('Strongpass5432!');
+
+    const user = User.create('user-id-123', email, password);
+
+    expect(user.email).toBe(email);
+    expect(user.email.value).toBe('bubu@hotmail.com');
+  });
+
+  it('should assign the password value object', () => {
+    const email = Email.create('user@email.com');
+    const password = Password.create('StrongPass123!');
+
+    const user = User.create('user-id-123', email, password);
+
+    expect(user.password).toBe(password);
+    expect(user.password.value).toBe('StrongPass123!');
+  });
+});
