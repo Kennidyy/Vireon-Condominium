aada0399b16d7bbe7c9fb64523b4a620f88010f9
Nikollas Kennidy <kennidynikollas.dev@gmail.com>
2026-07-24 16:37:05 -0300

    test(api/identity): add argon2 password hasher unit tests

--- src/apps/api/src/modules/identity/infrastructure/crypto/Argon2PasswordHasher.spec.ts
+++ src/apps/api/src/modules/identity/infrastructure/crypto/Argon2PasswordHasher.spec.ts
@@ -10,4 +10,15 @@ describe("Argon2 password hasher", () => {
         expect(hashedPassword).toBeDefined()
         expect(hashedPassword).not.toEqual(pswd)
     })
+
+    it("should compare a password with its hash", async () => {
+        const passwordHasher = new Argon2PasswordHasher()
+        const pswd = "Senha1234@#@"
+        const hashedPassword = await passwordHasher.hash(pswd)
+
+        const isMatch = await passwordHasher.compare(pswd, hashedPassword)
+
+        console.log("isMatch", isMatch)
+        expect(isMatch).toBe(true)
+    })
 })
