2bd7d09234fc7d1af3544bcdede462592f0e334b
Nikollas Kennidy <kennidynikollas.dev@gmail.com>
2026-07-24 16:39:55 -0300

    refactor(api/identity): refactor argon2 password hasher and unit tests

--- src/apps/api/src/modules/identity/infrastructure/crypto/Argon2PasswordHasher.spec.ts
+++ src/apps/api/src/modules/identity/infrastructure/crypto/Argon2PasswordHasher.spec.ts
@@ -1,24 +1,24 @@
-import { Argon2PasswordHasher } from "./Argon2PasswordHasher"
+import { Argon2PasswordHasher } from './Argon2PasswordHasher';
 
-describe("Argon2 password hasher", () => {
-    it("should hash a password", async () => {
-        const passwordHasher = new Argon2PasswordHasher()
-        const pswd = "Senha1234@#@"
-        const hashedPassword = await passwordHasher.hash(pswd)
-        console.log("hashedPassword", hashedPassword)
+describe('Argon2 password hasher', () => {
+  it('should hash a password', async () => {
+    const passwordHasher = new Argon2PasswordHasher();
+    const pswd = 'Senha1234@#@';
+    const hashedPassword = await passwordHasher.hash(pswd);
+    console.log('hashedPassword', hashedPassword);
 
-        expect(hashedPassword).toBeDefined()
-        expect(hashedPassword).not.toEqual(pswd)
-    })
+    expect(hashedPassword).toBeDefined();
+    expect(hashedPassword).not.toEqual(pswd);
+  });
 
-    it("should compare a password with its hash", async () => {
-        const passwordHasher = new Argon2PasswordHasher()
-        const pswd = "Senha1234@#@"
-        const hashedPassword = await passwordHasher.hash(pswd)
+  it('should compare a password with its hash', async () => {
+    const passwordHasher = new Argon2PasswordHasher();
+    const pswd = 'Senha1234@#@';
+    const hashedPassword = await passwordHasher.hash(pswd);
 
-        const isMatch = await passwordHasher.compare(pswd, hashedPassword)
+    const isMatch = await passwordHasher.compare(pswd, hashedPassword);
 
-        console.log("isMatch", isMatch)
-        expect(isMatch).toBe(true)
-    })
-})
+    console.log('isMatch', isMatch);
+    expect(isMatch).toBe(true);
+  });
+});
--- src/apps/api/src/modules/identity/infrastructure/crypto/Argon2PasswordHasher.ts
+++ src/apps/api/src/modules/identity/infrastructure/crypto/Argon2PasswordHasher.ts
@@ -1,12 +1,12 @@
-import * as argon2 from "argon2";
-import { PasswordHasher } from "../../application/ports/PasswordHasher";
+import * as argon2 from 'argon2';
+import { PasswordHasher } from '../../application/ports/PasswordHasher';
 
 export class Argon2PasswordHasher implements PasswordHasher {
-    async hash(pswd: string): Promise<string> {
-        return await argon2.hash(pswd);
-    }
+  async hash(pswd: string): Promise<string> {
+    return await argon2.hash(pswd);
+  }
 
-    async compare(pswd: string, hashed: string): Promise<boolean> {
-        return await argon2.verify(hashed, pswd);
-    }
+  async compare(pswd: string, hashed: string): Promise<boolean> {
+    return await argon2.verify(hashed, pswd);
+  }
 }
