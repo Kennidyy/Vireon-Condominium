883c59d7260255d5642bf7c3b3fceec2885013cd
Nikollas Kennidy <kennidynikollas.dev@gmail.com>
2026-07-24 16:07:46 -0300

    test(api): add Argon2 password hash unit test

---
+++ src/apps/api/src/modules/identity/infrastructure/crypto/Argon2PasswordHasher.spec.ts
@@ -0,0 +1,13 @@
+import { Argon2PasswordHasher } from "./Argon2PasswordHasher"
+
+describe("Argon2 password hasher", () => {
+    it("should hash a password", async () => {
+        const passwordHasher = new Argon2PasswordHasher()
+        const pswd = "Senha1234@#@"
+        const hashedPassword = await passwordHasher.hash(pswd)
+        console.log("hashedPassword", hashedPassword)
+
+        expect(hashedPassword).toBeDefined()
+        expect(hashedPassword).not.toEqual(pswd)
+    })
+})
